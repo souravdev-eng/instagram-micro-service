5950b82d8fc816b1d739863a0d63328c6db9b6eb
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2022-04-14 09:09:43 +0530

    Project created and setup skaffold.yml

---
+++ auth/src/index.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL!, {
+      user: process.env.USER_NAME!,
+      pass: process.env.DB_PASSWORD!,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+
+  app.listen(3000, () => {
+    console.log("Auth Service is running on port 3000");
+  });
+};
+
+start();
