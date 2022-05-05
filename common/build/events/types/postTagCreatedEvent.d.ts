47762d50bc61b9be3c0d23c94461c71d0048454d
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2022-05-05 11:20:21 +0530

    Updates

--- common/build/events/types/postTagCreatedEvent.d.ts
+++ common/build/events/types/postTagCreatedEvent.d.ts
@@ -1,1 +1,9 @@
-export {};
+import { Subjects } from "../subjects/Subjects";
+export interface PostTagCreatedEvent {
+    subject: Subjects.TagsCreated;
+    data: {
+        postId: string;
+        userId: string;
+        tags: string[] | string;
+    };
+}
